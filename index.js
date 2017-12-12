4d8ef96ef189068e87fe394b9ed8668215c652ff
abaileyjane <32373118+abaileyjane@users.noreply.github.com>
2017-12-12 01:16:28 -0500

    Revert "Carousel"

--- index.js
+++ index.js
@@ -16,26 +16,17 @@ $.getJSON(GUARDIAN_SEARCH_URL, query, callback);
 function displayGuardianArticles(data){
 	console.log(data, "displayGuardianArticles ran");
 	const results = data.response.results.map((item,index)=> renderGuardianResult(item));
-	$('.guardian-section-heading').html('<h2>The Guardian</h2>')
 	$('.guardian-results').append(results);
 }	
 
 function renderGuardianResult(result){
 	console.log("renderGuardianResult ran")
 	return `
-	<article class="guardian-article">
-		<div class="row">
-			<div class="col-6">
-				<img src="${result.fields.thumbnail}">
-			</div>
-
-			<div class="col-6">
-				<a href="${result.webUrl}">
-					<h3>${result.webTitle}</h3>
-				</a>
-			</div>
-		</div>
-	</article>
+	<article>
+		<a href="${result.webUrl}">
+		<img src="${result.fields.thumbnail}">
+			<h2>${result.webTitle}</h2>
+		</a>
 
 		`
 }
@@ -51,26 +42,22 @@ $.getJSON(IMAGE_SEARCH_URL, query, callback);
 
 function displayGoogleImage(data){
 	console.log(data, "displayGoogleImage ran");
-	$('.main-search-page').html(
-		'<div class="row" style="text-align: center">' +
-			
-				'<img id="large-image" src=""/>'+
-		
-		'</div>'+
-		'<div class="row">'+
-		'<div class="col-12" style="">'+
-			
-				'<ul class="slides"></ul>'+
-		'</div></div>')
-		;
-	for (let i=0; i<10; i++){
-		const imageUrl= data.items[i].pagemap.cse_image[0].src;
-			$('.slides').append(`<li class="slide" ><span class="col-1"><img class="thumbnail" src="${imageUrl}"/></span></li>`)}
-	$('#large-image').attr('src', data.items[0].pagemap.cse_image[0].src);
-	watchImageClick();
+	const results = data.items.map((item,index)=> renderGoogleImage(item));
+	$('.google-image-results').append(results);
+}	
+
+function renderGoogleImage(items){
+	console.log("renderGoogleImage ran")
+	return `
+	<article>
+		<a href="${items.link}">
+		<img src="${items.pagemap.cse_image[0].src}">
+			<h2>${items.title}</h2>
+			<h3>${items.snippet}</h3>
+		</a>
+
+		`
 }
-
-
 
 function getDataFromNYTimesApi(searchTerm, callback){
 	const query = {
@@ -83,42 +70,33 @@ $.getJSON(NYTIMES_SEARCH_URL, query, callback);
 function displayTimesArticles(data){
 	console.log(data, "displayTimesArticles ran");
 	const results = data.response.docs.map((item,index)=> renderTimesResult(item));
-	$('.ny-section-heading').html('<h2>The New York Times</h2>');
-	$('.new-york-times-results').append(results);}	
+	$('.new-york-times-results').append(results);
+}	
 
 function renderTimesResult(result){
 	console.log("renderTimesResult ran")
-	try {
-		return `
-	<article class="nyTimesArticle">
-		<div class="row">
-			<div class="col-12" style="text-align:right">
-				<a href="${result.web_url}">
-					<h3>${result.headline.main}</h3>
-				</a>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-6"
-				<a href="${result.web_url}">
-					<img class="thumbnail" src="http://www.nytimes.com/${result.multimedia[2].url}">
-				</a>
-			</div>
-			<div class="col-6">
-				<p>${result.snippet}</p>
-			</div>
-		</div>
-	</article>
+	return `
+	<article>
+		<a href="${result.web_url}">
+			<img class="thumbnail" src="http://www.nytimes.com/${result.multimedia[2].url}">
+			<h2>${result.headline.main}</h2>
+		</a>
+		<h3>${result.snippet}</h3>
 		`
-	}
-
-	catch(err) {
-		return('<h3> No results found</h3>')
-	}
 }
 
-
+//function getDataFromGoogleImageApi(searchTerm, callback){
+//	console.log("getDataFromGoogleImageApi ran");
+//	const query = {
+//		searchType = "image",
+//		part: 'snippet',
+//		q: `${searchTerm}`,
+//		key: 'AIzaSyCctJU2Yz0r1F6LdJUdC92s5BXEws0lFP8'
+//		per_page: 20
+//	};
+//	$.getJSON()
+//	}
+//}
 
 function watchSubmit(){
 	$(".submit-button").click(event=>{
@@ -126,24 +104,18 @@ function watchSubmit(){
 		event.preventDefault();
 		const query = $(".keyword").val();
 		console.log(query);
-		$(".main").prop('hidden', false);
 		getDataFromNYTimesApi(query, displayTimesArticles);
 		getDataFromGuardianApi(query, displayGuardianArticles);
 		getDataFromGoogleImageApi(query, displayGoogleImage);
-		$('.search-term-container').html(`<h1> ${query} </h1>`)
-		
+
+
 	})
 }
 
-function watchImageClick(){
-	console.log('watchImageClick ran');
-	$(".thumbnail").on('click', function(event){
-		event.preventDefault();
-		const thumbnailUrl= $(this).attr('src');
-		console.log('the click registered!! this is the URL', thumbnailUrl);
-		$('#large-image').attr('src', thumbnailUrl);
-	})
+function link(){
+	console.log("this is linked");
 }
 
+$(link);
 
-$(document).ready(watchSubmit)
+$(watchSubmit);
